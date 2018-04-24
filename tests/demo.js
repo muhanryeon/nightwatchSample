aabd83e5fb08ae16cf40003ee0a720e19ee5b12c
jhchoi <jhchoi@zigbang.com>
2018-04-24 11:31:30 +0900

    check

--- tests/demo.js
+++ tests/demo.js
@@ -7,7 +7,7 @@ module.exports = {
             .waitForElementVisible('button[name=btnG]', 1000)
             .click('button[name=btnG]')
             .pause(1000)
-            .assert.containsText('#main', 'Night Watch')
+            .assert.containsText('#main', 'Night Watch Demo')
             .end();
     }
 };
